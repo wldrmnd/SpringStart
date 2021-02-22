33b8b48c30ed96fc0fe61b88e3bc4a9875201a92
Egor Barkovsky <woldermind@gmail.com>
2021-02-22 21:21:12 +0300

    task

--- src/main/java/com/wldrmnd/startspring/StartspringApplication.java
+++ src/main/java/com/wldrmnd/startspring/StartspringApplication.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//посмотреть уроки spring + thymeleaf
+//понять что такое рест, сделать по гайду стагндартное приложение на Spring MVC
+//https://www.youtube.com/playlist?app=desktop&list=PLU2ftbIeotGoGSEUf54LQH-DgiQPF2XRO
+//посмотреть видосы про спринг кор
 @SpringBootApplication
 public class StartspringApplication {
 
